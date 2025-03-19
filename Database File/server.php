603bfa67015f1f45beb9f53a7f04f6ca144bbce0
ETHICAL HACKER <cyberboy.sann@gmail.com>
2025-03-20 02:10:22 +0530

    server.php

--- Database File/server.php
+++ Database File/server.php
@@ -1,12 +1,8 @@
-<?php
-session_start();
-
-// initializing variables
 $username = "";
 $email    = "";
 $errors = array(); 
 
-// connect to the database
+
 $db = mysqli_connect('localhost', 'root', '', 'project');
 
 // REGISTER USER
@@ -17,8 +13,7 @@ if (isset($_POST['reg_user'])) {
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
+
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
@@ -26,8 +21,8 @@ if (isset($_POST['reg_user'])) {
 	array_push($errors, "The two passwords do not match");
   }
 
-  // first check the database to make sure 
-  // a user does not already exist with the same username and/or email
+  // First check the database to make sure 
+  // A user does not already exist with the same username and/or email
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
