6954d60fec0a561798c5f49cc9a0604bd8eae40f
ETHICAL HACKER <cyberboy.sann@gmail.com>
2025-03-20 05:10:16 +0530

    server.php

--- Database File/server.php
+++ Database File/server.php
@@ -5,6 +5,6 @@ $errors = array();
 to be continue.........
 cyberboy will update the logical code of the server today by evening...
 
-------------------------
+-----------------------
 "Stay Agile, Stay Safe!"--SANN
-------------------------
+-----------------------
