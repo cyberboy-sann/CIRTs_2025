2916d40e3afe8a8d68862c2e0e072b4169d24ba6
ETHICAL HACKER <cyberboy.sann@gmail.com>
2025-04-21 21:28:42 +0530

    Create server.php

---
+++ Front-End File/server.php
@@ -0,0 +1,10 @@
+$username = "";
+$email    = "";
+$errors = array(); 
+
+to be continue.........
+cyberboy will update the logical code of the server soon.....
+
+-----------------------
+"Stay Agile, Stay Safe!"--SANN
+-----------------------
